3ed896fdc683005442e0db5e80db7b7325e4fd6a
Bb <142775487+Wassamapol@users.noreply.github.com>
2024-08-01 10:09:19 +0700

    new db

--- add.php
+++ add.php
@@ -8,6 +8,7 @@
         body {
             font-family: Arial, sans-serif;
             margin: 20px;
+            background-color : gray;
         }
         table {
             width: 100%;
@@ -26,6 +27,8 @@
         tr:nth-child(even) {
             background-color: #f2f2f2;
         }
+
+
     </style>
 </head>
 <body>
@@ -48,41 +51,47 @@
     $sql = "SELECT * FROM customers";
     $query = mysqli_query($conn, $sql);
     ?>
-    <table>
-        <tr>
-            <th>CustomerID</th>
-            <th>CompanyName</th>
-            <th>ContactName</th>
-            <th>ContactTitle</th>
-            <th>Address</th>
-            <th>City</th>
-            <th>Region</th>
-            <th>PostalCode</th>
-            <th>Country</th>
-            <th>Phone</th>
-            <th>Fax</th>
-        </tr>
-        <?php
-        while ($result = mysqli_fetch_assoc($query)) {
-            ?>
-            <tr>
-                <td><?php echo $result["CustomerID"]; ?></td>
-                <td><?php echo $result["CompanyName"]; ?></td>
-                <td><?php echo $result["ContactName"]; ?></td>
-                <td><?php echo $result["ContactTitle"]; ?></td>
-                <td><?php echo $result["Address"]; ?></td>
-                <td><?php echo $result["City"]; ?></td>
-                <td><?php echo $result["Region"]; ?></td>
-                <td><?php echo $result["PostalCode"]; ?></td>
-                <td><?php echo $result["Country"]; ?></td>
-                <td><?php echo $result["Phone"]; ?></td>
-                <td><?php echo $result["Fax"]; ?></td>
-            </tr>
-        <?php
-        }
-        ?>
-    </table>
+    <form method="post" action="<?php echo $_SERVER['PHP_SELF'];?>">
+        
+        CustomerID<input type="text" name"CustomerID"><br>
+        CompanyName :<input type="text" name"CompanyName"> <br>
+        ContactName : <input type="text" name"ContactName"> <br>
+        ContactTitle :   <input type="text" name"ContactTitle"> <br>
+        Address:    <input type="text" name"Address"> <br>
+        City:    <input type="text" name"City"> <br>
+        Region:    <input type="text" name"Region"> <br>
+        PostalCode:    <input type="text" name"PostalCode"> <br>
+        Country:    <input type="text" name"Country"> <br>
+        Phone:    <input type="text" name"Phone"> <br>
+        Fax:    <input type="text" name"Fax"> <br>
+           
+        <input type="submit" value="ส่งข้อมูล" >
+     
+    </form>
     <?php
+    mysqli_close($conn);
+    ?>
+    <? php 
+    if ($ $_SERVER["REQUEST_METHOD"]="POST") {
+        $customerID = mysqli_real_escape_string($conn, $_POST["CustomerID"]);
+        $CompanyName = mysqli_real_escape_string($conn, $_POST["CompanyName"]);
+        $ContactName = mysqli_real_escape_string($conn, $_POST["ContactName"]);
+        $ContactTitle = mysqli_real_escape_string($conn, $_POST["ContactTitle"]);
+        $Address = mysqli_real_escape_string($conn, $_POST["Address"]);
+        $City = mysqli_real_escape_string($conn, $_POST["City"]);
+        $Region = mysqli_real_escape_string($conn, $_POST["Region"]);
+        $PostalCode = mysqli_real_escape_string($conn, $_POST["PostalCode"]);
+        $Country = mysqli_real_escape_string($conn, $_POST["Country"]);
+        $Phone = mysqli_real_escape_string($conn, $_POST["Phone"]);
+        $Fax = mysqli_real_escape_string($conn, $_POST["Fax"]);
+         
+        $msql = "INSERT INTO customers (CustomerID ,CompanyName, ContactName,ContactTitle ,Address, City,Region ,Postalcode,PostalCode, COuntry, Phone, Fax) VALUES ('$customerID' , '$CompanyName', '$ContactTitle','$Address','$City','$Region', '$PostalCode','$Country','$Phone','$Fax') ";
+    }
+    if (mysqli_query($conn, $msql)) {
+        echo "Created successfully" ;
+    } else {
+        echo "Error : " . $msql ." <br>"  mysqli_error($conn);
+    }
     mysqli_close($conn);
     ?>
 </body>
